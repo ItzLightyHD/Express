e8c1c018640b90128c110e16b3d71e827728d2c8
DylanOfDiscord <72952471+DylanOfDiscord@users.noreply.github.com>
2020-11-05 23:17:06 -0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    "token": process.env.TOKEN,
-    "prefix": process.env.PREFIX,
-    "owners": [process.env.OWNERID],
-    "host": process.env.HOST,
-    "hostpassw": process.env.HOSTPASSW
-}
